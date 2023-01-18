3829c4f474e79f0b0af4be8ed68a77f9099e9c8d
rodAlfaro-98 <rodrigoalfarod@gmail.com>
2023-01-17 19:55:59 -0600

    Se eliminaron los prints de depuración

--- naiveBayes.py
+++ naiveBayes.py
@@ -16,29 +16,23 @@ total = total_hate + total_no_hate
 p_hate = total_hate/total
 p_no_hate = total_no_hate/total
 
-print('NO HATE')
-message = 'racism is power'
+message = 'billionaire chair of trump says he hopes obama dies and michelle goes back to being a male ape in africa.'
+print(message)
 #message_processed = message_preprocessing(message)
 message_processed = message
-print(message_processed)
 sum = 0
 for i in message_processed.split(' '):
     if(i in no_hate['word'].values.tolist()):
         count = no_hate[no_hate['word'] == i]['count'].values[0]
         sum += log(count/total_no_hate)*count
-        print(i,sum)
 p_no_hate_message = log(p_no_hate) + sum
 
-print('HATE')
 sum = 1
 for i in message_processed.split(' '):
     if(i in hate['word'].values.tolist()):
         count = hate[hate['word'] == i]['count'].values[0]
         sum += log(count/total_no_hate)*count
-        print(i,sum)
 p_hate_message = log(p_hate) + sum
-
-print(p_hate_message, p_no_hate_message)
 
 if(p_hate_message < p_no_hate_message):
     print("Es un tweet de odio")
